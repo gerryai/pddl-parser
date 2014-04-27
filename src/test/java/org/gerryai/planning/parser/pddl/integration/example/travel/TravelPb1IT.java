4b35ef0675b6960a4e7c2ca143233695f6798fe6
David Edwards <david@more.fool.me.uk>
2014-04-27 12:56:26 +0100

    Completed integration tests for the Travel domain

---
+++ src/test/java/org/gerryai/planning/parser/pddl/integration/example/travel/TravelPb1IT.java
@@ -0,0 +1,221 @@
+package org.gerryai.planning.parser.pddl.integration.example.travel;
+
+import org.gerryai.planning.model.ConstantDefinition;
+import org.gerryai.planning.model.problem.Goal;
+import org.gerryai.planning.parser.pddl.integration.ProblemSuccessTester;
+import org.junit.Test;
+
+import static org.gerryai.planning.model.logic.FormulaBuilder.and;
+import static org.gerryai.planning.model.logic.FormulaBuilder.constant;
+import static org.gerryai.planning.model.logic.FormulaBuilder.predicate;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Integration test to check that the travel example files are parsed correctly.
+ */
+public class TravelPb1IT extends ProblemSuccessTester {
+
+    protected String getFilePath() {
+        return "pddl/example/travel/pb1.pddl";
+    }
+
+    @Test
+    public void travelProblem1HasCorrectName() {
+        assertEquals("big-bull1", problem.getName());
+    }
+
+    @Test
+    public void travelProblem1HasCorrectDomain() {
+        assertEquals("bulldozer", problem.getDomainName());
+    }
+
+    @Test
+    public void travelProblem1Has9Objects() {
+        assertEquals(9, problem.getObjects().asSet().size());
+    }
+
+    @Test
+    public void travelProblem1HasObjectA() {
+        assertTrue(problem.getObjects().asSet().contains(new ConstantDefinition("a")));
+    }
+
+    @Test
+    public void travelProblem1HasObjectB() {
+        assertTrue(problem.getObjects().asSet().contains(new ConstantDefinition("b")));
+    }
+
+    @Test
+    public void travelProblem1HasObjectC() {
+        assertTrue(problem.getObjects().asSet().contains(new ConstantDefinition("c")));
+    }
+
+    @Test
+    public void travelProblem1HasObjectD() {
+        assertTrue(problem.getObjects().asSet().contains(new ConstantDefinition("d")));
+    }
+
+    @Test
+    public void travelProblem1HasObjectE() {
+        assertTrue(problem.getObjects().asSet().contains(new ConstantDefinition("e")));
+    }
+
+    @Test
+    public void travelProblem1HasObjectF() {
+        assertTrue(problem.getObjects().asSet().contains(new ConstantDefinition("f")));
+    }
+
+    @Test
+    public void travelProblem1HasObjectG() {
+        assertTrue(problem.getObjects().asSet().contains(new ConstantDefinition("g")));
+    }
+
+    @Test
+    public void travelProblem1HasObjectJack() {
+        assertTrue(problem.getObjects().asSet().contains(new ConstantDefinition("jack")));
+    }
+
+    @Test
+    public void travelProblem1HasObjectBulldozer() {
+        assertTrue(problem.getObjects().asSet().contains(new ConstantDefinition("bulldozer")));
+    }
+
+    @Test
+    public void travelProblem1Has25StateFormulas() {
+        assertEquals(25, problem.getInitialState().asSet().size());
+    }
+
+    @Test
+    public void travelProblem1InitialStateHasAtJackA() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("at", constant("jack"), constant("a"))));
+    }
+
+    @Test
+    public void travelProblem1InitialStateHasAtBulldozerE() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("at", constant("bulldozer"), constant("e"))));
+    }
+
+    @Test
+    public void travelProblem1InitialStateHasVehicleBulldozer() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("vehicle", constant("bulldozer"))));
+    }
+
+    @Test
+    public void travelProblem1InitialStateHasMobileJack() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("mobile", constant("jack"))));
+    }
+
+    @Test
+    public void travelProblem1InitialStateHasPersonJack() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("person", constant("jack"))));
+    }
+
+    @Test
+    public void travelProblem1InitialStateHasRoadAB() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("road", constant("a"), constant("b"))));
+    }
+
+    @Test
+    public void travelProblem1InitialStateHasRoadBA() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("road", constant("b"), constant("a"))));
+    }
+
+    @Test
+    public void travelProblem1InitialStateHasRoadAE() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("road", constant("a"), constant("e"))));
+    }
+
+    @Test
+    public void travelProblem1InitialStateHasRoadEA() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("road", constant("e"), constant("a"))));
+    }
+
+    @Test
+    public void travelProblem1InitialStateHasRoadEB() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("road", constant("e"), constant("b"))));
+    }
+
+    @Test
+    public void travelProblem1InitialStateHasRoadBE() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("road", constant("b"), constant("e"))));
+    }
+
+    @Test
+    public void travelProblem1InitialStateHasRoadAC() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("road", constant("a"), constant("c"))));
+    }
+
+    @Test
+    public void travelProblem1InitialStateHasRoadCA() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("road", constant("c"), constant("a"))));
+    }
+
+    @Test
+    public void travelProblem1InitialStateHasRoadCB() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("road", constant("c"), constant("b"))));
+    }
+
+    @Test
+    public void travelProblem1InitialStateHasRoadBC() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("road", constant("b"), constant("c"))));
+    }
+
+    @Test
+    public void travelProblem1InitialStateHasBridgeBD() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("bridge", constant("b"), constant("d"))));
+    }
+
+    @Test
+    public void travelProblem1InitialStateHasBridgeDB() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("bridge", constant("d"), constant("b"))));
+    }
+
+    @Test
+    public void travelProblem1InitialStateHasBridgeCF() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("bridge", constant("c"), constant("f"))));
+    }
+
+    @Test
+    public void travelProblem1InitialStateHasBridgeFC() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("bridge", constant("f"), constant("c"))));
+    }
+
+    @Test
+    public void travelProblem1InitialStateHasRoadDF() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("road", constant("d"), constant("f"))));
+    }
+
+    @Test
+    public void travelProblem1InitialStateHasRoadFD() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("road", constant("f"), constant("d"))));
+    }
+
+    @Test
+    public void travelProblem1InitialStateHasRoadFG() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("road", constant("f"), constant("g"))));
+    }
+
+    @Test
+    public void travelProblem1InitialStateHasRoadGF() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("road", constant("g"), constant("f"))));
+    }
+
+    @Test
+    public void travelProblem1InitialStateHasRoadDG() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("road", constant("d"), constant("g"))));
+    }
+
+    @Test
+    public void travelProblem1InitialStateHasRoadGD() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("road", constant("g"), constant("d"))));
+    }
+
+    @Test
+    public void travelProblem1HasGoal() {
+        Goal goal = new Goal(
+                and(
+                        predicate("at", constant("bulldozer"), constant("g")),
+                        predicate("at", constant("jack"), constant("a"))));
+        assertEquals(goal, problem.getGoal());
+    }
+}
---
+++ src/test/java/org/gerryai/planning/parser/pddl/integration/example/travel/TravelPb2IT.java
@@ -0,0 +1,314 @@
+package org.gerryai.planning.parser.pddl.integration.example.travel;
+
+import org.gerryai.planning.model.ConstantDefinition;
+import org.gerryai.planning.model.problem.Goal;
+import org.gerryai.planning.parser.pddl.integration.ProblemSuccessTester;
+import org.junit.Test;
+
+import static org.gerryai.planning.model.logic.FormulaBuilder.and;
+import static org.gerryai.planning.model.logic.FormulaBuilder.constant;
+import static org.gerryai.planning.model.logic.FormulaBuilder.predicate;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Integration test to check that the travel example files are parsed correctly.
+ */
+public class TravelPb2IT extends ProblemSuccessTester {
+
+    protected String getFilePath() {
+        return "pddl/example/travel/pb2.pddl";
+    }
+
+    @Test
+    public void travelProblem2HasCorrectName() {
+        assertEquals("big-bull2", problem.getName());
+    }
+
+    @Test
+    public void travelProblem2HasCorrectDomain() {
+        assertEquals("bulldozer", problem.getDomainName());
+    }
+
+    @Test
+    public void travelProblem2Has16Objects() {
+        assertEquals(16, problem.getObjects().asSet().size());
+    }
+
+    @Test
+    public void travelProblem2HasObjectA() {
+        assertTrue(problem.getObjects().asSet().contains(new ConstantDefinition("a")));
+    }
+
+    @Test
+    public void travelProblem2HasObjectB() {
+        assertTrue(problem.getObjects().asSet().contains(new ConstantDefinition("b")));
+    }
+
+    @Test
+    public void travelProblem2HasObjectC() {
+        assertTrue(problem.getObjects().asSet().contains(new ConstantDefinition("c")));
+    }
+
+    @Test
+    public void travelProblem2HasObjectD() {
+        assertTrue(problem.getObjects().asSet().contains(new ConstantDefinition("d")));
+    }
+
+    @Test
+    public void travelProblem2HasObjectE() {
+        assertTrue(problem.getObjects().asSet().contains(new ConstantDefinition("e")));
+    }
+
+    @Test
+    public void travelProblem2HasObjectF() {
+        assertTrue(problem.getObjects().asSet().contains(new ConstantDefinition("f")));
+    }
+
+    @Test
+    public void travelProblem2HasObjectG() {
+        assertTrue(problem.getObjects().asSet().contains(new ConstantDefinition("g")));
+    }
+
+    @Test
+    public void travelProblem2HasObjectH() {
+        assertTrue(problem.getObjects().asSet().contains(new ConstantDefinition("h")));
+    }
+
+    @Test
+    public void travelProblem2HasObjectI() {
+        assertTrue(problem.getObjects().asSet().contains(new ConstantDefinition("i")));
+    }
+
+    @Test
+    public void travelProblem2HasObjectJ() {
+        assertTrue(problem.getObjects().asSet().contains(new ConstantDefinition("j")));
+    }
+
+    @Test
+    public void travelProblem2HasObjectK() {
+        assertTrue(problem.getObjects().asSet().contains(new ConstantDefinition("k")));
+    }
+
+    @Test
+    public void travelProblem2HasObjectL() {
+        assertTrue(problem.getObjects().asSet().contains(new ConstantDefinition("l")));
+    }
+
+    @Test
+    public void travelProblem2HasObjectM() {
+        assertTrue(problem.getObjects().asSet().contains(new ConstantDefinition("m")));
+    }
+
+    @Test
+    public void travelProblem2HasObjectN() {
+        assertTrue(problem.getObjects().asSet().contains(new ConstantDefinition("n")));
+    }
+
+
+    @Test
+    public void travelProblem2HasObjectJack() {
+        assertTrue(problem.getObjects().asSet().contains(new ConstantDefinition("jack")));
+    }
+
+    @Test
+    public void travelProblem2HasObjectBulldozer() {
+        assertTrue(problem.getObjects().asSet().contains(new ConstantDefinition("bulldozer")));
+    }
+
+    @Test
+    public void travelProblem2Has37StateFormulas() {
+        assertEquals(37, problem.getInitialState().asSet().size());
+    }
+
+    @Test
+    public void travelProblem2InitialStateHasAtJackA() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("at", constant("jack"), constant("a"))));
+    }
+
+    @Test
+    public void travelProblem2InitialStateHasAtBulldozerE() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("at", constant("bulldozer"), constant("e"))));
+    }
+
+    @Test
+    public void travelProblem2InitialStateHasVehicleBulldozer() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("vehicle", constant("bulldozer"))));
+    }
+
+    @Test
+    public void travelProblem2InitialStateHasMobileJack() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("mobile", constant("jack"))));
+    }
+
+    @Test
+    public void travelProblem2InitialStateHasPersonJack() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("person", constant("jack"))));
+    }
+
+    @Test
+    public void travelProblem2InitialStateHasRoadAB() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("road", constant("a"), constant("b"))));
+    }
+
+    @Test
+    public void travelProblem2InitialStateHasRoadBA() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("road", constant("b"), constant("a"))));
+    }
+
+    @Test
+    public void travelProblem2InitialStateHasRoadAC() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("road", constant("a"), constant("c"))));
+    }
+
+    @Test
+    public void travelProblem2InitialStateHasRoadCA() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("road", constant("c"), constant("a"))));
+    }
+
+    @Test
+    public void travelProblem2InitialStateHasRoadCD() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("road", constant("c"), constant("d"))));
+    }
+
+    @Test
+    public void travelProblem2InitialStateHasRoadDC() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("road", constant("d"), constant("c"))));
+    }
+
+    @Test
+    public void travelProblem2InitialStateHasRoadDE() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("road", constant("d"), constant("e"))));
+    }
+
+    @Test
+    public void travelProblem2InitialStateHasRoadED() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("road", constant("e"), constant("d"))));
+    }
+
+    @Test
+    public void travelProblem2InitialStateHasRoadEJ() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("road", constant("e"), constant("j"))));
+    }
+
+    @Test
+    public void travelProblem2InitialStateHasRoadJE() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("road", constant("j"), constant("e"))));
+    }
+
+    @Test
+    public void travelProblem2InitialStateHasRoadDF() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("road", constant("d"), constant("f"))));
+    }
+
+    @Test
+    public void travelProblem2InitialStateHasRoadFD() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("road", constant("f"), constant("d"))));
+    }
+
+    @Test
+    public void travelProblem2InitialStateHasRoadFJ() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("road", constant("f"), constant("j"))));
+    }
+
+    @Test
+    public void travelProblem2InitialStateHasRoadJF() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("road", constant("j"), constant("f"))));
+    }
+
+    @Test
+    public void travelProblem2InitialStateHasRoadFK() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("road", constant("f"), constant("k"))));
+    }
+
+    @Test
+    public void travelProblem2InitialStateHasRoadKF() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("road", constant("k"), constant("f"))));
+    }
+
+    @Test
+    public void travelProblem2InitialStateHasRoadJH() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("road", constant("j"), constant("h"))));
+    }
+
+    @Test
+    public void travelProblem2InitialStateHasRoadHJ() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("road", constant("h"), constant("j"))));
+    }
+
+    @Test
+    public void travelProblem2InitialStateHasRoadHK() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("road", constant("h"), constant("k"))));
+    }
+
+    @Test
+    public void travelProblem2InitialStateHasRoadKH() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("road", constant("k"), constant("h"))));
+    }
+
+    @Test
+    public void travelProblem2InitialStateHasBridgeKL() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("bridge", constant("k"), constant("l"))));
+    }
+
+    @Test
+    public void travelProblem2InitialStateHasBridgeLK() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("bridge", constant("l"), constant("k"))));
+    }
+
+    @Test
+    public void travelProblem2InitialStateHasBridgeKN() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("bridge", constant("k"), constant("n"))));
+    }
+
+    @Test
+    public void travelProblem2InitialStateHasBridgeNK() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("bridge", constant("n"), constant("k"))));
+    }
+
+    @Test
+    public void travelProblem2InitialStateHasRoadLM() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("road", constant("l"), constant("m"))));
+    }
+
+    @Test
+    public void travelProblem2InitialStateHasRoadML() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("road", constant("m"), constant("l"))));
+    }
+
+    @Test
+    public void travelProblem2InitialStateHasRoadMN() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("road", constant("m"), constant("n"))));
+    }
+
+    @Test
+    public void travelProblem2InitialStateHasRoadNM() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("road", constant("n"), constant("m"))));
+    }
+
+    @Test
+    public void travelProblem2InitialStateHasRoadMG() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("road", constant("m"), constant("g"))));
+    }
+
+    @Test
+    public void travelProblem2InitialStateHasRoadGM() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("road", constant("g"), constant("m"))));
+    }
+
+    @Test
+    public void travelProblem2InitialStateHasRoadNG() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("road", constant("n"), constant("g"))));
+    }
+
+    @Test
+    public void travelProblem2InitialStateHasRoadGN() {
+        assertTrue(problem.getInitialState().asSet().contains(predicate("road", constant("g"), constant("n"))));
+    }
+
+    @Test
+    public void travelProblem2HasGoal() {
+        Goal goal = new Goal(and(predicate("at", constant("bulldozer"), constant("g"))));
+        assertEquals(goal, problem.getGoal());
+    }
+}
